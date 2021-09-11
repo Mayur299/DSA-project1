73032e8a13e4ef9bb634d5d08cf321a1391016f9
Mayur Kumar <mayur310209@gmail.com>
2021-09-11 14:07:35 +0530

    lfu final

--- lfu.cpp
+++ lfu.cpp
@@ -117,7 +117,7 @@ class lfu
         }
     }
 
-    void put(int key,int value)
+    void set(int key,int value)
     {
            if (cap==0)
            return;
@@ -156,22 +156,61 @@ class lfu
 
 };
 
+
+  
 int main()
-{  
-   lfu *l=new lfu(2);
-   l->put(1,1);
-   l->put(2,2);
-   cout<<l->get(1)<<endl;
-   l->put(3,3);
-   cout<<l->get(2)<<endl;
-   cout<<l->get(3)<<endl;
-   l->put(4,4);
-  cout<<l->get(1)<<endl;
-   cout<<l->get(3)<<endl;
-   cout<<l->get(4)<<endl;
+{
+    lfu* exp;
 
+    while(1)
+    {
+      int c;
+      cin>>c;
+      switch (c)
+      {
+          case 1:
+          int k;
+          cin>>k;
+          exp=new lfu(k);
+          break;
 
+          case 2:
+          int key;
+          cin>>key;
+          cout<<exp->get(key)<<endl;
+          break;
 
+          case 3:
+          int ke;
+          int ve;
+          cin>>ke>>ve;
+          exp->set(ke,ve);
+          break;
+
+          case -1:
+          exit(0);
+          break;
+
+          default :
+          cout<<"invalid choice"<<endl;
+          
+
+      }
+      
+    }
+
+   /* exp->set(1,10);
+    exp->set(2,20);
+    exp->set(3,30);
+    cout<<exp->get(2)<<endl;
+    exp->set(4,40);
+    cout<<exp->get(1)<<endl;
+    cout<<exp->get(3)<<endl;
+    cout<<exp->get(2)<<endl;
+    cout<<exp->get(4)<<endl;
+    exp->set(1,10);*/
+    
 
     return 0;
 }
+
