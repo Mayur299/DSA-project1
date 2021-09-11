72cdac046e17b97defe9ef4922d2800de8e0b51c
Mayur Kumar <mayur310209@gmail.com>
2021-09-11 14:08:11 +0530

    lru final

--- new.cpp
+++ new.cpp
@@ -57,6 +57,7 @@ class lru
 
      int get(int k)
      {
+         
          if(m.find(k)==m.end())
          return -1;
          ll *temp=remove(m[k]);
@@ -68,7 +69,12 @@ class lru
      }
 
      void set(int k,int v)
-     {
+     {   if(cap==0)
+         {
+             int sorry=0;
+         }
+         else
+         {
          if(m.find(k)==m.end())
          {
              if(m.size()==cap)
@@ -79,7 +85,6 @@ class lru
     
          
              m.erase(temp->key);
-             free(tempn);
          }
 
 
@@ -95,10 +100,11 @@ class lru
          else
          {
              ll* temp=remove(m[k]);
+             temp->value=v;
              insert(temp);
 
          }
-
+         }
          
 
 
@@ -115,8 +121,53 @@ class lru
 
 int main()
 {
-    lru* exp=new lru(1);
-    exp->set(1,10);
+    lru* exp;
+
+    while(1)
+    {
+      int c;
+      cin>>c;
+      switch (c)
+      {
+          case 1:
+          int k;
+          cin>>k;
+          exp=new lru(k);
+          break;
+
+          case 2:
+          int key;
+          cin>>key;
+          cout<<exp->get(key)<<endl;
+          break;
+
+          case 3:
+          int ke;
+          int ve;
+          cin>>ke>>ve;
+          exp->set(ke,ve);
+          break;
+
+          case -1:
+          exit(0);
+          break;
+
+          default :
+          cout<<"invalid choice"<<endl;
+          
+
+      }
+      {
+      //case /* constant-expression */:
+          /* code */
+          //break;
+      
+      //default:
+          //break;
+      }
+    }
+
+   /* exp->set(1,10);
     exp->set(2,20);
     exp->set(3,30);
     cout<<exp->get(2)<<endl;
@@ -125,7 +176,7 @@ int main()
     cout<<exp->get(3)<<endl;
     cout<<exp->get(2)<<endl;
     cout<<exp->get(4)<<endl;
-    exp->set(1,10);
+    exp->set(1,10);*/
     
 
     return 0;
